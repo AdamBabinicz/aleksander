c398918193dc39f86aa689a0a43400e0d296d0e3
babinicz <babinicz@onet.pl>
2021-11-20 15:38:42 +0100

    zm wielkości fontów w footer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,18 +2,38 @@ import React, { useState, useEffect } from "react";
 import logo from "../images/3.svg";
 import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+import { animateScroll as scroll } from "react-scroll";
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   const [click, setClick] = useState(false);
 
   const handleClick = () => setClick(!click);
   const closeMenu = () => setClick(false);
 
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const changeNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
   return (
     <>
-      <div className="header">
+      <div className="header" scrollNav={scrollNav}>
         <div className="navbar">
-          <a href="#start" className="logo">
+          <a href="#start" className="logo" onClick={toggleHome}>
             <img src={logo} alt="..." />
           </a>
           <div className="hamburger" onClick={handleClick}>
@@ -24,13 +44,29 @@ const Navbar = () => {
             )}
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
-            <li className="nav-item">
-              <a href="#start" onClick={closeMenu}>
+            <li className="nav-item" onClick={closeMenu}>
+              <Link
+                to="/"
+                onClick={toggleHome}
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
                 Start
-              </a>
+              </Link>
             </li>
-            <li className="nav-item">
-              <a href="#strateg" onClick={closeMenu}>
+            <li className="nav-item" onClick={closeMenu}>
+              <a
+                href="#strateg"
+                onClick={toggleHome}
+                smooth={true}
+                duration={500}
+                spy={true}
+                exact="true"
+                offset={-80}
+              >
                 Strateg
               </a>
             </li>
